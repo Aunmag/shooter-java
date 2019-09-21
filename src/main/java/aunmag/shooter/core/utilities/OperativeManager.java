5d14929224870a3b1b08fe1fc427ed64a38ae566
Aunmag <aunmag@gmail.com>
2019-09-21 14:33:23 +0500

    Fixed rendering of collided bullets

--- src/main/java/aunmag/shooter/core/utilities/OperativeManager.java
+++ src/main/java/aunmag/shooter/core/utilities/OperativeManager.java
@@ -7,13 +7,17 @@ public class OperativeManager<T extends Operative> {
 
     public final List<T> all = new ArrayList<>();
 
+    /**
+     * Note that if an object has became inactive after updating, it will only be removed
+     * during the next iteration to render the object once before it is removed.
+     */
     public void update() {
         for (var i = all.size() - 1; i >= 0; i--) {
             var operative = all.get(i);
 
-            operative.update();
-
-            if (!operative.isActive()) {
+            if (operative.isActive()) {
+                operative.update();
+            } else {
                 operative.remove();
                 all.remove(i);
             }
